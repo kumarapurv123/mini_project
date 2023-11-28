72a14d43115dd3a77c8741f4a686da38439e347d
kumarapurv123 <91779441+kumarapurv123@users.noreply.github.com>
2023-11-28 12:06:07 +0530

    Add files via upload

---
+++ timeTable.js
@@ -0,0 +1,123 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'AB-108',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'AB-109',
+        subject: 'BCSS001',
+        type: 'Tut'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'AB-111',
+        subject: 'BCSC0002',
+        type: 'Lecture'
+    }
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'AB-108',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+  
+    {   
+        time: '12-01 PM',
+        roomNumber: 'AB-111',
+        subject: 'BCSC0002',
+        type: 'Lecture'
+    }
+]
+
+const Wednesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'AB-108',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'AB-109',
+        subject: 'BCSS001',
+        type: 'Tut'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'AB-111',
+        subject: 'BCSC0002',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'AB-108',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'AB-109',
+        subject: 'BCSS001',
+        type: 'Tut'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'AB-111',
+        subject: 'BCSC0002',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'AB-108',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'AB-109',
+        subject: 'BCSS001',
+        type: 'Tut'
+    },
+    {   
+        time: '12-01 PM',
+        roomNumber: 'AB-111',
+        subject: 'BCSC0002',
+        type: 'Lecture'
+    }
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'AB-108',
+        subject: 'DBMS130',
+        type: 'Lecture'
+    },
+   
+    {   
+        time: '12-01 PM',
+        roomNumber: 'AB-111',
+        subject: 'BCSC0002',
+        type: 'Lecture'
+    }
+]
